147fc4f0fbb401bfac6ce3d4b6b4d332eae96131
Ben Hughes <ben@railsgarden.com>
2011-08-07 15:17:46 -0700

    Some application defaults.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module BluePassers
   class Application < Rails::Application
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework  :rspec, :fixture => true, :views => false
+      g.integration_tool :rspec, :fixture => true, :views => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
     
     config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
