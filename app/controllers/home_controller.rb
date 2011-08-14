3385e9def049925d031d8d990521f340f6a6e3a7
Ben Hughes <ben@railsgarden.com>
2011-08-14 15:05:31 -0700

    Added 'Fork me on Github' ribbon and made header h1 a link to homepage.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     @leaderboard_users = User.leaderboard
     @recent_check_ins = CheckIn.recent
+
+    flash[:notice] = "Note: The BluePass doesn't begin until August 22nd. We'll begin tracking flights then! "
   end
 end
